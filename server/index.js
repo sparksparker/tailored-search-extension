e88b302b0a0d3bc9abb60f0d83d1d4b07c3bb0ab
sparksparker <sehyunspark@gmail.com>
2019-09-24 20:34:53 -0700

    Add favicon

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 const port = 3000;
 const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World'));
+// app.use(favicon(path.join(__dirname, '../client/dist')));
+
+app.use(favicon(path.join(__dirname, '../client/dist', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
