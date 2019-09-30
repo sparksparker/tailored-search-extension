f94d019bcc7d5d1af673b97dcac687c8bf6c4c44
sparksparker <sehyunspark@gmail.com>
2019-09-29 22:57:23 -0700

    Remove comments

--- server/index.js
+++ server/index.js
@@ -4,10 +4,6 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-// app.use(favicon(path.join(__dirname, '../client/dist')));
-
-// app.use(favicon(path.join(__dirname, '../client/dist', 'favicon.ico')));
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.listen(port, () => console.log(`Listening on PORT ${port}`));
